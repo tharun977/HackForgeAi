e8314d5711aab318d4e0af0b32a3cc0c79ef17ae
Tharun <tharunraman10@gmail.com>
2025-04-23 22:01:29 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // next.config.js
 module.exports = {
+  experimental: {
+    serverActions: true,
+  },
+  
   env: {
     CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API,
     CLERK_API_KEY: process.env.CLERK_API_KEY,
